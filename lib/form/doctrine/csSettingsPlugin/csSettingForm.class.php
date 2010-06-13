c172a5638dfb8c4f43511d80c7f0c5778a100c59
kryptadia <kryptadia@gmail.com>
2010-06-13 15:30:36 +0000

    ajout du plugin csSettingsPlugin

---
+++ lib/form/doctrine/csSettingsPlugin/csSettingForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * csSetting form.
+ *
+ * @package    copisim
+ * @subpackage form
+ * @author     Pierre-FrançoisPilouAngrand
+ * @version    SVN: $Id: sfDoctrinePluginFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class csSettingForm extends PlugincsSettingForm
+{
+  public function configure()
+  {
+  }
+}
