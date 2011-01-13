71b69c949bd7b98491c37e96ed52b4431ec20414
kryptadia@gmail.com <kryptadia@gmail.com@0f6db53d-2fc9-fea4-28c6-0af26ee42e7b>
2011-01-13 13:59:03 +0000

    mise à jour de symfony

--- trunk/lib/form/doctrine/base/BaseCopisimEtudiantForm.class.php
+++ trunk/lib/form/doctrine/base/BaseCopisimEtudiantForm.class.php
@@ -8,7 +8,7 @@
  * @package    copisim
  * @subpackage form
  * @author     Pierre-FrançoisPilouAngrand
- * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
  */
 abstract class BaseCopisimEtudiantForm extends BaseFormDoctrine
 {
@@ -29,7 +29,7 @@ abstract class BaseCopisimEtudiantForm extends BaseFormDoctrine
     ));
 
     $this->setValidators(array(
-      'id'         => new sfValidatorDoctrineChoice(array('model' => $this->getModelName(), 'column' => 'id', 'required' => false)),
+      'id'         => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
       'nom'        => new sfValidatorString(array('max_length' => 50)),
       'prenom'     => new sfValidatorString(array('max_length' => 50)),
       'fac'        => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('CopisimFac'))),
--- trunk/lib/form/doctrine/base/BaseCopisimFluxForm.class.php
+++ trunk/lib/form/doctrine/base/BaseCopisimFluxForm.class.php
@@ -8,7 +8,7 @@
  * @package    copisim
  * @subpackage form
  * @author     Pierre-FrançoisPilouAngrand
- * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
  */
 abstract class BaseCopisimFluxForm extends BaseFormDoctrine
 {
@@ -23,7 +23,7 @@ abstract class BaseCopisimFluxForm extends BaseFormDoctrine
     ));
 
     $this->setValidators(array(
-      'id'         => new sfValidatorDoctrineChoice(array('model' => $this->getModelName(), 'column' => 'id', 'required' => false)),
+      'id'         => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
       'periode'    => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('CopisimPeriode'))),
       'ville'      => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('CopisimRegion'))),
       'complement' => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('CopisimFiliere'))),
